2d5949d4fdae042bfd406228c724c16c2c9741f7
JVBruno <jeanvincent.bruno@viacesi.fr>
2023-06-30 01:37:04 +0200

    added  OrderDashboard

--- microservice_commande/routes/index.js
+++ microservice_commande/routes/index.js
@@ -88,6 +88,20 @@ app.post('/orders', createOrderHandler);
 // Gestionnaire pour la modification d'une commande
 app.put('/orders/:orderId', updateOrderHandler);
 
+
+app.get('/all-orders', getAllOrdersHandler);
+
+
+async function getAllOrdersHandler(req, res) {
+  try {
+    const orders = await Order.find({});
+    res.json(orders);
+  } catch (error) {
+    console.error('Error fetching all orders:', error);
+    res.status(500).json({ message: 'Server error while fetching all orders' });
+  }
+}
+
 //Gestionnaire pour la suppresion d'une commande
 app.delete('/orders/:orderId', deleteOrderHandler);
 
